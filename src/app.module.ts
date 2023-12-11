47df94647408c81340331d5e882bd2e5cddaf601
ezygas <ezygas@gmail.com>
2023-12-11 09:48:12 +0100

    Added caching with redis.
    Added globally working  autocaching logic. It works on all endpoints.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
-import { PlanetsModule } from './planets/planets.module';
 import { FilmsModule } from './films/films.module';
 import { SpeciesModule } from './species/species.module';
 import { PeopleModule } from './people/people.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { StarshipsModule } from './starships/starships.module';
+import { PlanetsModule } from './planets/planets.module';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
+    CacheModule.register<RedisClientOptions>({
+      isGlobal: true,
+      ttl: 60 * 60 * 24,
+      store: redisStore,
+      socket: {
+        host: 'redis',
+        port: 6379,
+      },
+    }),
     FilmsModule,
     SpeciesModule,
     PeopleModule,
@@ -16,6 +29,11 @@ import { StarshipsModule } from './starships/starships.module';
     PlanetsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
